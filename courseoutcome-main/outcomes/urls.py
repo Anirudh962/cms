d21fd2d9a28b03f3814a01406f44c3c020cd8015
Anirudh962 <anirudhs962@gmail.com>
2024-12-17 14:28:21 +0530

    first commit

--- courseoutcome-main/outcomes/urls.py
+++ courseoutcome-main/outcomes/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('course_outcomes/', views.outcomes, name='outcomes'),
-    path('course_outcomes/insertco/',views.insertco, name='insertco'),
+    path('home/', views.home, name='home'),
+    path('home/insertco/',views.insertco, name='insertco'),
     path('view_co/',views.view_co,name='view_co'),
+    path('deleteprofile/<str:course_name>/', views.deleteprofile, name='deleteprofile'),
+    path('editprofile/<str:course_name>/', views.editprofile, name='editprofile'),
+    path('updateco/<str:course_name>/', views.updateco, name='updateco'),
 ]
--- courseoutcome-main/outcomes/views.py
+++ courseoutcome-main/outcomes/views.py
@@ -4,8 +4,8 @@ from django.views.decorators.csrf import csrf_protect
 
 # Create your views here.
 @csrf_protect
-def outcomes(request):
-    return render(request,"course_outcomes.html",{})
+def home(request):
+    return render(request,"home.html",{'request': request})
 
 def insertco(request):
     vtucname= request.POST['tucname']; 
@@ -19,12 +19,29 @@ def insertco(request):
 def view_co(request):
     # Fetch all courses from the database
     courses = course_outcomes.objects.all()
-    return render(request, 'course_outcomes.html', {'courses_outcomes': courses})
+    return render(request, 'home.html', {'courses_outcomes': courses})
 
-def deleteprofile(request,tucname):
-    courses_name=course_outcomes.object.get(course_name=tucname)
+def deleteprofile(request,course_name):
+    courses_name=course_outcomes.objects.get(course_name=course_name)
     courses_name.delete()
-    return redirect("viewco")
+    return redirect("view_co")
+
+def editprofile(request, course_name):
+  course = course_outcomes.objects.get(course_name=course_name)
+  return render(request,"editprofile.html",{'course':course})
+
+def updateco(request, course_name):
+    ntucname= request.POST['tucname']; 
+    ntudes = request.POST['tudes'];
+    ntuchours = request.POST['tuchours'];
+    ntumarks = request.POST['tumarks'];
+    co = course_outcomes.objects.get(course_name=course_name)
+    co.course_name=ntucname
+    co.description=ntudes
+    co.contact_hours=ntuchours
+    co.marks=ntumarks
+    co.save();
+    return redirect('view_co')
 
 
 
